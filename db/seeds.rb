cdbdaa10a575cbebbd51b92c57c4fabe917cda48
Alexis Gonzalez <1991.alexis@gmail.com>
2021-10-20 18:13:15 -0400

    Seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Flat.create!(
+    name: Faker::FunnyName.four_word_name,
+    address: Faker::Address.street_address,
+    description: Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    price_per_night: rand(20..150),
+    number_of_guests: rand(1..7)
+  )
+end
